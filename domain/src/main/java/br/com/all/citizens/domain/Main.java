5b1c53d57d3baa868085b6af03e696547b1f1f7f
spockerman <campos.sandro@gmail.com>
2025-04-02 14:06:20 -0300

    update folder structure of project

--- domain/src/main/java/br/com/all/citizens/domain/Main.java
+++ domain/src/main/java/br/com/all/citizens/domain/Main.java
@@ -1,4 +1,4 @@
-package br.com.all.citizens;
+package br.com.all.citizens.domain;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
